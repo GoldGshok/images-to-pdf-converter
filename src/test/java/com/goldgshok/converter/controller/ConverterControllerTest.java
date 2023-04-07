9ad2ad82372478c038066753020d11ea221a5672
GoldGshok <goldgshok@gmail.com>
2023-04-07 17:02:43 +0700

    refactor

--- src/test/java/com/goldgshok/converter/controller/ConverterControllerTest.java
+++ src/test/java/com/goldgshok/converter/controller/ConverterControllerTest.java
@@ -1,9 +1,11 @@
 package com.goldgshok.converter.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(ConverterController.class)
 class ConverterControllerTest {
 
     @Test
