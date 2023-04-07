ce1b648f5bc07eecd2f2d7c88c90367545b1afc1
GoldGshok <goldgshok@gmail.com>
2023-04-07 17:04:43 +0700

    controller test

--- src/test/java/com/goldgshok/converter/controller/ConverterControllerTest.java
+++ src/test/java/com/goldgshok/converter/controller/ConverterControllerTest.java
@@ -1,10 +1,19 @@
 package com.goldgshok.converter.controller;
 
+import com.goldgshok.converter.service.ConverterService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ConverterController.class)
 class ConverterControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private ConverterService converterService;
 
     @Test
     void convert() {
