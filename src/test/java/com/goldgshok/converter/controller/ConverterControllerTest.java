0d28609a7b617b46603a49d29f793572021a9969
GoldGshok <goldgshok@gmail.com>
2023-04-07 16:51:00 +0700

    refactor

---
+++ src/test/java/com/goldgshok/converter/controller/ConverterControllerTest.java
@@ -0,0 +1,10 @@
+package com.goldgshok.converter.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void convert() {
+    }
+}
