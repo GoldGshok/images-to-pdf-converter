54262fccecbe5b23a660011baf9b3eb8f51c87f5
GoldGshok <goldgshok@gmail.com>
2023-04-07 17:22:24 +0700

    controller test

--- src/test/java/com/goldgshok/converter/controller/ConverterControllerTest.java
+++ src/test/java/com/goldgshok/converter/controller/ConverterControllerTest.java
@@ -1,26 +1,13 @@
 package com.goldgshok.converter.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goldgshok.converter.BaseControllerTest;
-import com.goldgshok.converter.SpringBootBaseTest;
 import com.goldgshok.converter.request.ConvertRequest;
 import com.goldgshok.converter.service.ConverterService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ConverterControllerTest extends BaseControllerTest {
 
